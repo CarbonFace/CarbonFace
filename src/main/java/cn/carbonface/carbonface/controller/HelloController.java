2a7fc7974a8a9b30495af31ccf155d9bf33cc16c
CarbonFace <553127022@qq.com>
2021-03-29 12:05:08 +0800

    whole project pom management update and common modules basic codes introduced.
    2021-3-29 12:01:17  Xiaoyi Ye

---
+++ src/main/java/cn/carbonface/carbonface/controller/HelloController.java
@@ -0,0 +1,27 @@
+package cn.carbonface.carbonface.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Classname HelloController
+ * @Description TODO
+ * @Author CarbonFace <553127022@qq.com>
+ * @Date 2021/3/26 14:01
+ * @Version V1.0
+ */
+
+@RestController
+@RequestMapping("/user")
+@Slf4j
+public class HelloController {
+
+
+    @GetMapping("hello")
+    public String hello(){
+        log.info("hello!");
+        return "hello";
+    }
+}
